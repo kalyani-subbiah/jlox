9392d7c66ab67652292ce9b6c8a2521b5ff88597
kalyani-subbiah <skalyani103@gmail.com>
2022-04-04 09:21:41 -0400

    upto Functions

---
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,93 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox 
+{
+  private static final Interpreter interpreter = new Interpreter();
+  static boolean hadError=false;
+  static boolean hadRuntimeError = false;
+  
+  public static void main(String[] args) throws IOException
+  {
+    
+    if (args.length > 1) 
+    {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64);
+    } else if (args.length == 1)
+    {
+      runFile(args[0]);
+    } else {
+      System.out.println("Running JLOX in REPL mode...");
+      runPrompt();
+    }
+  }
+
+  private static void runFile(String path) throws IOException
+  {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+  }
+
+  private static void runPrompt() throws IOException 
+  {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+    int y = 7;
+    for (;;) 
+    {
+      System.out.print("> ");
+      String line = reader.readLine();
+      // Ctrl + D signals EOF and readLine returns null, so check for that. 
+      if (line == null) break;
+      run(line);
+      hadError = false; 
+    }
+  }
+
+  private static void run (String source)
+  {
+    if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
+    
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+    
+    Parser parser = new Parser(tokens);
+    List<Stmt> statements = parser.parse();
+    // Stop if there's a syntax error
+    if (hadError) return;
+
+    Resolver resolver = new Resolver(interpreter);
+    resolver.resolve(statements);
+    // Stop if there was a resolution error.
+    if (hadError) return;
+    
+    interpreter.interpret(statements);
+  }
+
+  static void error(int line, String message)
+  {
+    report(line, "", message);
+  }
+  
+  static void runtimeError(RuntimeError error) 
+  {
+	    System.err.println(error.getMessage() +
+	        "\n[line " + error.token.line + "]");
+	    hadRuntimeError = true;
+  }
+
+  private static void report(int line, String where, String message)
+  {
+    System.err.println("[line " + line + "] Error" + where + ":" + message);
+    hadError = true;
+  }
+}
